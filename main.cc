fa61cd800b96945c942a63e4b243dcf0f9316a02
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-07-15 17:01:26 -0300

    Generate a 'sky' background

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
-#include "Vec3.hh"
+#include "Ray.hh"
 
 using namespace std;
+
+Vec3 bg_color(const Ray& r);
 
 int main() {
     int nx = 200;
@@ -9,12 +11,21 @@ int main() {
     
     cout << "P3\n" << nx << " " << ny << "\n255\n";
 
+    Vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    Vec3 horizontal(4.0, 0.0, 0.0);
+    Vec3 vertical(0.0, 2.0, 0.0);
+    Vec3 origin(0.0, 0.0, 0.0);
+
     for (int j = ny-1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
-            // RGB values from 0.0 to 1.0
-            Vec3 color(float(i) / float(nx), // RED increases from left to right
-                       float(j) / float(ny), // GREEN decreases from top to bottom
-                       0.2);                 // BLUE is fixed at 20%
+            float u = float(i) / float(nx); // increases from left to right
+            float v = float(j) / float(ny); // decreases from top to bottom
+            
+            Vec3 direction = lower_left_corner + u*horizontal + v*vertical;
+            Ray r(origin, direction);
+            
+            Vec3 color = bg_color(r); // RGB values from 0.0 to 1.0
+
             // mapping [0.0, 1.0] to [0, 255]
             int ir = int(255.99*color.r());
             int ig = int(255.99*color.g());
@@ -22,4 +33,13 @@ int main() {
             cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+}
+
+Vec3 bg_color(const Ray& r) {
+    Vec3 unit_direction = unit_vector(r.direction());
+    // mapping [-1.0, 1.0] to [0.0, 1.0]
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    
+    // blends white and blue based on the ray's y coordinate
+    return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0);
 }
--- vec3.hh
+++ vec3.hh
@@ -9,7 +9,7 @@ class Vec3 {
     public:
         float e[3];
 
-        Vec3() {}
+        Vec3() { }
         Vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
 
         inline float x() const { return e[0]; }
