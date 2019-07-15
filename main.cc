22e30967885718ca3f913d16610792b736f8f0f0
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-07-15 20:19:42 -0300

    Solid color a sphere

--- main.cc
+++ main.cc
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-Vec3 bg_color(const Ray& r);
+bool hit_sphere(const Vec3& center, float radius, const Ray& r);
+Vec3 visible_color(const Ray& r);
 
 int main() {
     int nx = 200;
@@ -24,7 +25,7 @@ int main() {
             Vec3 direction = lower_left_corner + u*horizontal + v*vertical;
             Ray r(origin, direction);
             
-            Vec3 color = bg_color(r); // RGB values from 0.0 to 1.0
+            Vec3 color = visible_color(r); // RGB values from 0.0 to 1.0
 
             // mapping [0.0, 1.0] to [0, 255]
             int ir = int(255.99*color.r());
@@ -35,7 +36,24 @@ int main() {
     }
 }
 
-Vec3 bg_color(const Ray& r) {
+bool hit_sphere(const Vec3& center, float radius, const Ray& r) {
+    Vec3 o_c = r.origin() - center; // A - C
+    float a = dot(r.direction(), r.direction()); // <B, B>
+    float b = 2.0 * dot(r.direction(), o_c); // <B, A - C>
+    float c = dot(o_c, o_c) - radius*radius; // <A - C, A - C> - R*R
+    // Sphere equation, with center C:
+    //   <p(t) - C, p(t) - C> = R*R
+    //   -> <A + t*B - C, A + t*B - C> = R*R
+    //   -> t*t*<B, B> + 2*t*<B, A - C> + <A - C, A - C> - R*R = 0
+    float discriminant = b*b - 4*a*c;
+    return discriminant > 0;
+}
+
+Vec3 visible_color(const Ray& r) {
+    if (hit_sphere(Vec3(0, 0, -1), 0.5, r)) {
+        return Vec3(1, 0, 0); // color the sphere RED
+    }
+
     Vec3 unit_direction = unit_vector(r.direction());
     // mapping [-1.0, 1.0] to [0.0, 1.0]
     float t = 0.5 * (unit_direction.y() + 1.0);
