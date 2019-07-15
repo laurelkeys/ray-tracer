27c38c1b7f9b0761126c7739b581e2ba1899203c
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-07-15 15:49:04 -0300

    Test Vec3

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Vec3.hh"
 
 using namespace std;
 
@@ -7,17 +8,17 @@ int main() {
     int ny = 100;
     
     cout << "P3\n" << nx << " " << ny << "\n255\n";
+
     for (int j = ny-1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
             // RGB values from 0.0 to 1.0
-            float r = float(i) / float(nx); // RED increases from left to right
-            float g = float(j) / float(ny); // GREEN decreases from top to bottom
-            float b = 0.2; // BLUE is fixed at 20%
-
+            Vec3 color(float(i) / float(nx), // RED increases from left to right
+                       float(j) / float(ny), // GREEN decreases from top to bottom
+                       0.2);                 // BLUE is fixed at 20%
             // mapping [0.0, 1.0] to [0, 255]
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
+            int ir = int(255.99*color.r());
+            int ig = int(255.99*color.g());
+            int ib = int(255.99*color.b());
             cout << ir << " " << ig << " " << ib << "\n";
         }
     }
--- vec3.hh
+++ vec3.hh
@@ -40,4 +40,116 @@ public:
     }
 };
 
+// extraction operator ("write")
+inline std::istream& operator>>(std::istream &is, Vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+// insertion operator ("read")
+inline std::ostream& operator<<(std::ostream &os, const Vec3 &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+    return os;
+}
+
+inline void Vec3::make_unit_vector() {
+    float k = 1.0 / length();
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
+}
+
+inline Vec3 operator+(const Vec3 &v1, const Vec3 &v2) {
+    return Vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline Vec3 operator-(const Vec3 &v1, const Vec3 &v2) {
+    return Vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+// elementwise multiplication
+inline Vec3 operator*(const Vec3 &v1, const Vec3 &v2) {
+    return Vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+// elementwise division
+inline Vec3 operator/(const Vec3 &v1, const Vec3 &v2) {
+    return Vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+// scalar multiplication
+inline Vec3 operator*(float t, const Vec3 &v) {
+    return Vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+inline Vec3 operator*(const Vec3 &v, float t) {
+    return Vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+// scalar division
+inline Vec3 operator/(Vec3 v, float t) {
+    return Vec3(v.e[0]/t, v.e[1]/t, v.e[2]/t);
+}
+
+// dot product (<v1, v2>)
+inline float dot(const Vec3 &v1, const Vec3 &v2) {
+    return v1.e[0] *v2.e[0] + v1.e[1] *v2.e[1]  + v1.e[2] *v2.e[2];
+}
+
+// cross product (v1 x v2)
+inline Vec3 cross(const Vec3 &v1, const Vec3 &v2) {
+    return Vec3((v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),  // v1.y * v2.z - v1.z * v2.y
+                (v1.e[2]*v2.e[0] - v1.e[0]*v2.e[2]),  // v1.z * v2.x - v1.x * v2.z
+                (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0])); // v1.x * v2.y - v1.y * v2.x
+}
+
+inline Vec3& Vec3::operator+=(const Vec3 &v){
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+}
+
+inline Vec3& Vec3::operator-=(const Vec3& v) {
+    e[0] -= v.e[0];
+    e[1] -= v.e[1];
+    e[2] -= v.e[2];
+    return *this;
+}
+
+// elementwise multiplication
+inline Vec3& Vec3::operator*=(const Vec3 &v){
+    e[0] *= v.e[0];
+    e[1] *= v.e[1];
+    e[2] *= v.e[2];
+    return *this;
+}
+
+// elementwise division
+inline Vec3& Vec3::operator/=(const Vec3 &v){
+    e[0] /= v.e[0];
+    e[1] /= v.e[1];
+    e[2] /= v.e[2];
+    return *this;
+}
+
+// scalar multiplication
+inline Vec3& Vec3::operator*=(const float t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+// scalar division
+inline Vec3& Vec3::operator/=(const float t) {
+    e[0] /= t;
+    e[1] /= t;
+    e[2] /= t;
+    return *this;
+}
+
+inline Vec3 unit_vector(Vec3 v) {
+    return v / v.length();
+}
+
 #endif
