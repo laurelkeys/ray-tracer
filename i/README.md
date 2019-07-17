7c8465a3621ab526712ad3eb7451e1435d775d21
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-07-16 23:07:56 -0300

    Try displaying images in i/README.md

---
+++ i/README.md
@@ -0,0 +1,9 @@
+|:---:|:---:|:---:|
+|![](image.png)|![](blue.png)|![](red_sphere.png)|
+|![](color_map.png)|![](two_spheres.png)|![](two_spheres_aa.png)|
+|![](matte_sphere_50.png)|![](matte_sphere_50_gamma2.png)|![](matte_sphere_50_gamma2_tmin001.png)|
+|![](matte_sphere_50_tmin001.png)|![](metal_spheres_depth50.png)|![](metal_spheres_depth5.png)|
+|![](metal_fuzz_reflection.png)|![](glass_refraction.png)|![](glass_refraction_no_blue.png)|
+|![](glass_refraction_no_blue_fresnel.png)|![](glass_refraction_fresnel.png)|![](hollow_glass_05thickness.png)|
+|![](hollow_glass_25thickness.png)|![](hollow_glass_10thickness.png)|![](hollow_glass_15thickness.png)|
+|![](hollow_glass_20thickness.png)|![](hollow_glass_45thickness.png)|![](bubble.png)|
---
+++ i/png2readme.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# ref.: 
+# https://stackoverflow.com/questions/168409/how-do-you-get-a-directory-listing-sorted-by-creation-date-in-python/539024#539024
+# https://docs.python.org/2/library/stat.html#stat.ST_CTIME
+
+from stat import S_ISREG, ST_CTIME, ST_MODE
+import os, sys, time, math
+import numpy as np
+
+# path to the directory (relative or absolute)
+dirpath = sys.argv[1] if len(sys.argv) == 2 else r'.'
+
+# get all entries in the directory w/ stats
+entries = (os.path.join(dirpath, fn) for fn in os.listdir(dirpath))
+entries = ((os.stat(path), path) for path in entries)
+
+# leave only .png files and insert creation date
+entries = [(stat[ST_CTIME], path) for stat, path in entries if S_ISREG(stat[ST_MODE]) and path.endswith('.png')]
+
+# NOTE: on Windows `ST_CTIME` is the creation date
+entries = sorted(entries)
+
+# the README table has 3 columns by default
+cols = sys.argv[2] if len(sys.argv) == 3 else 3
+
+def readme_fname(path):
+    return f"![]({os.path.basename(path)})"
+
+table = np.array([readme_fname(path) for _, path in entries])
+# print(f"{table.size} entries, {cols} columns, {math.ceil(table.size / cols)} rows")
+
+table = table.reshape((-1, cols))
+print("|:---:" * cols + '|')
+for row in table:
+    print('|' + '|'.join(row) + '|')
