f6c5a7aa886a8416d8442fe3e76f18f0cc322053
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-12-15 21:18:25 -0300

    Use Andrew Kensler's AABB hit method

--- src/AABB.hh
+++ src/AABB.hh
@@ -3,11 +3,6 @@
 
 #include "Ray.hh"
 
-// note: by not worrying about NaN and other exceptions these 
-//       are a bit faster than the built-in fmin() and fmax()
-inline float ffmin(float a, float b) { return a < b ? a : b; }
-inline float ffmax(float a, float b) { return a > b ? a : b; }
-
 class AABB { // Axis-Aligned Bounding Box
     public:
         Vec3 _min;
@@ -21,20 +16,18 @@ class AABB { // Axis-Aligned Bounding Box
         Vec3 min() const { return _min; }
         Vec3 max() const { return _max; }
 
-        bool hit(const Ray& r, float t_min, float t_max) const {
-            for (int a = 0; a < 3; ++a) {
-                float t0 = ffmin((_min[a] - r.origin()[a]) / r.direction()[a],
-                                 (_max[a] - r.origin()[a]) / r.direction()[a]);
-                float t1 = ffmax((_min[a] - r.origin()[a]) / r.direction()[a],
-                                 (_max[a] - r.origin()[a]) / r.direction()[a]);
-                
-                t_min = ffmax(t0, t_min);
-                t_max = ffmin(t1, t_max);
-
+        inline bool AABB::hit(const Ray& r, float t_min, float t_max) const {
+            for (int a = 0; a < 3; a++) {
+                float invD = 1.0f / r.direction()[a];
+                float t0 = (min()[a] - r.origin()[a]) * invD;
+                float t1 = (max()[a] - r.origin()[a]) * invD;
+                if (invD < 0.0f)
+                    std::swap(t0, t1);
+                t_min = t0 > t_min ? t0 : t_min;
+                t_max = t1 < t_max ? t1 : t_max;
                 if (t_max <= t_min)
                     return false;
             }
-
             return true;
         }
 };
