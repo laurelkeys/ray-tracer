38b31f8345617d12daff82c669d0850d63809afd
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-12-17 23:34:19 -0300

    Implement BVH

--- src/AABB.hh
+++ src/AABB.hh
@@ -35,18 +35,18 @@ class AABB { // Axis-Aligned Bounding Box
             }
             return true;
         }
-
-        AABB surrounding_box(AABB box0, AABB box1) {
-            Vec3 small(
-                    ffmin(box0.min().x(), box1.min().x()),
-                    ffmin(box0.min().y(), box1.min().y()),
-                    ffmin(box0.min().z(), box1.min().z()));
-            Vec3 big(
-                    ffmax(box0.max().x(), box1.max().x()),
-                    ffmax(box0.max().y(), box1.max().y()),
-                    ffmax(box0.max().z(), box1.max().z()));
-            return AABB(small, big);
-        }
 };
+
+AABB surrounding_box(AABB box0, AABB box1) {
+    Vec3 small(
+            ffmin(box0.min().x(), box1.min().x()),
+            ffmin(box0.min().y(), box1.min().y()),
+            ffmin(box0.min().z(), box1.min().z()));
+    Vec3 big(
+            ffmax(box0.max().x(), box1.max().x()),
+            ffmax(box0.max().y(), box1.max().y()),
+            ffmax(box0.max().z(), box1.max().z()));
+    return AABB(small, big);
+}
 
 #endif
